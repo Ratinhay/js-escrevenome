54724b8ebedeb09aa0c78e46b2444ac86e24892b
Yasmin Santos <00001131366335SP@al.educacao.sp.gov.br>
2024-06-03 20:47:06 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+    createCanvas(600, 600);
+    background("white");
+  }
+  
+  function draw() {
+    
+    
+    stroke("blue")
+    fill ("black")
+    
+    // console.log(mouseIsPressed);
+    
+    if (mouseIsPressed) {
+    rect(mouseX,mouseY,25,35);
+    }
+  }  
+    
+  
